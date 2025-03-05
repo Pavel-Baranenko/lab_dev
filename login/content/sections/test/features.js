225364012109bdc05e57bb006edc6f1a4396bd29
Pavel-Baranenko <baranenko_work@mail.ru>
2025-03-05 12:44:28 +0300

    update

--- login/content/sections/test/features.js
+++
@@ -1,9 +0,0 @@
-
-
-function preventDefaultForm_lab_sign_form_989() {
-        const lab_sign_form_989 = document.querySelector('#lab_sign_form_989')
-    lab_sign_form_989.addEventListener('submit', e => {
-        e.preventDefault()
-    })
-    }
-preventDefaultForm_lab_sign_form_989()
--- login/temp/content/sections/test/features.js
+++
@@ -1,9 +0,0 @@
-console.log("test инициализировано")
-
-function preventDefaultForm_lab_sign_form_136() {
-        const lab_sign_form_136 = document.querySelector('#lab_sign_form_136')
-    lab_sign_form_136.addEventListener('submit', e => {
-        e.preventDefault()
-    })
-    }
-preventDefaultForm_lab_sign_form_136()
